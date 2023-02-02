56b04662d4f43c18114aeaf227cd7f04e7281bb7
tc-hakamata <hakamata.timecapsule@gmail.com>
2023-02-02 18:04:22 +0900

    Install ESLint

--- App.jsx
+++ App.jsx
@@ -1,10 +1,14 @@
+/* eslint-disable react/style-prop-object */
+/* eslint-disable react/react-in-jsx-scope */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
     </View>
   );
