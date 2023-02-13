e5a555071aa7f1c340418f13aa865105c0fd43c1
tc-hakamata <hakamata.timecapsule@gmail.com>
2023-02-13 13:57:39 +0900

    Fetch data from firestore on MemoDetailScreen

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { format } from 'date-fns';
+
+export function dateToString(date) {
+  if (!date) { return ''; }
+  return format(date, 'yyyy年M月d日 HH時mm分');
+}
